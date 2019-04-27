8fc1f954bd4e88af4230683fdcd7d07afa96a563
bprafferty03 <bprafferty03@gmail.com>
2019-04-27 14:07:03 -0700

    created singleton game manager

---
+++ Assets/Scenes/gameManaging.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManaging : MonoBehaviour {
+    public static gameManaging gameInstance {
+        get;
+        private set;
+    }
+
+    public int savedScore;
+
+    private void Awake() {
+        if (gameInstance == null) {
+            Debug.Log("Saved Score is " + savedScore);
+            gameInstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
