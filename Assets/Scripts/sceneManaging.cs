03526cdb4dabd56b38d5ac580e09ab35096d31b2
bprafferty03 <bprafferty03@gmail.com>
2019-04-27 15:02:31 -0700

    added functionality to quit button

--- Assets/Scripts/sceneManaging.cs
+++ Assets/Scripts/sceneManaging.cs
@@ -19,8 +19,11 @@ public class sceneManaging : MonoBehaviour {
         SceneManager.LoadScene("Test_Scene1");
         gameManaging.gameInstance.savedScore++;
     }
-    public void goToMain(){
+    public void goToMain() {
         SceneManager.LoadScene("Main Menu");
         gameManaging.gameInstance.savedScore = 0;
+    }
+    public void quitGame() {
+        Application.Quit();
     }
 }
