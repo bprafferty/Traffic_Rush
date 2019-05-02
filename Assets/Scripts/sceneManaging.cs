415d4738fd1074dcf230d6f340f6a2df447a564c
bprafferty03 <bprafferty03@gmail.com>
2019-05-01 21:25:22 -0700

    created in-game menu system

--- Assets/Scripts/sceneManaging.cs
+++ Assets/Scripts/sceneManaging.cs
@@ -7,22 +7,44 @@ using UnityEngine.UI;
 public class sceneManaging : MonoBehaviour {
     public Text scoreText;
 
+    public GameObject pauseMenu;
+
+    void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name != "Main Menu") {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0.0f;
+            Debug.Log("Time is: " + Time.timeScale);
+        }
+
+    }
+
+    public void setTime(){
+        Time.timeScale = 1.0f;
+        Debug.Log("Time is: " + Time.timeScale);
+    }
+
     private void Start() {
         scoreText.text = gameManaging.gameInstance.savedScore.ToString();
     }
+
+
     public void goToNextScene() {
         int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextLevel);
         gameManaging.gameInstance.savedScore++;
     }
+
     public void goToLevelOne() {
         SceneManager.LoadScene("Test_Scene1");
         gameManaging.gameInstance.savedScore++;
     }
+
     public void goToMain() {
         SceneManager.LoadScene("Main Menu");
         gameManaging.gameInstance.savedScore = 0;
     }
+
     public void quitGame() {
         Application.Quit();
     }
