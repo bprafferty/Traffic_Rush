389d9c67bf2a314024e4001f3709f7b5874fd161
bprafferty03 <bprafferty03@gmail.com>
2019-05-05 12:35:23 -0700

    created score system that incremements by time

--- Assets/Scripts/sceneManaging.cs
+++ Assets/Scripts/sceneManaging.cs
@@ -6,17 +6,35 @@ using UnityEngine.UI;
 
 public class sceneManaging : MonoBehaviour {
     public Text scoreText;
+    public int currentScore = 0;
+
 
     public GameObject pauseMenu;
 
     void Update() {
-        
-        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name != "Main Menu") {
+        scoreText.text = gameManaging.gameInstance.savedScore.ToString();
+        //if (SceneManager.GetActiveScene().name == "Test_Scene1"){
+
+        //}
+
+
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name != "Main Menu"){
             pauseMenu.SetActive(true);
             Time.timeScale = 0.0f;
             Debug.Log("Time is: " + Time.timeScale);
+            StopCoroutine(scoreTimer());
+        }
+        if (pauseMenu.activeSelf == false && SceneManager.GetActiveScene().name != "Main Menu") {
+            StartCoroutine(scoreTimer());
         }
 
+    }
+
+    private IEnumerator scoreTimer() {
+        while (SceneManager.GetActiveScene().name != "Main Menu") {
+            gameManaging.gameInstance.savedScore += 1;
+            yield return new WaitForSecondsRealtime(100.0f);
+        }
     }
 
     public void setTime(){
@@ -25,24 +43,24 @@ public class sceneManaging : MonoBehaviour {
     }
 
     private void Start() {
-        scoreText.text = gameManaging.gameInstance.savedScore.ToString();
+        
     }
 
 
     public void goToNextScene() {
+        gameManaging.gameInstance.savedScore = 0;
         int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextLevel);
-        gameManaging.gameInstance.savedScore++;
+        //gameManaging.gameInstance.savedScore++;
     }
 
     public void goToLevelOne() {
         SceneManager.LoadScene("Test_Scene1");
-        gameManaging.gameInstance.savedScore++;
+        //gameManaging.gameInstance.savedScore++;
     }
 
     public void goToMain() {
         SceneManager.LoadScene("Main Menu");
-        gameManaging.gameInstance.savedScore = 0;
     }
 
     public void quitGame() {
