d8156802f551063c009dd3a734c0a2095c600fb0
colin5374 <colinwaters5374@gmail.com>
2019-05-05 12:51:42 -0700

    added score to main menu and gameover scene

---
+++ Assets/Scripts/highscoreScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class highscoreScript : MonoBehaviour
+{
+    public Text highscoreText;
+    private void Start()
+    {
+        highscoreText.text = "Highscore: " + gameManaging.gameInstance.savedScore.ToString();
+    }
+}
